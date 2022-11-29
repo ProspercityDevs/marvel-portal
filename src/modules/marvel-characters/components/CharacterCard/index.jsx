ba2a8a576d4dadd66428500da88791f9b4758bfb
CamiloGiraldoM <camiloandresgiraldo000@gmail.com>
2022-11-29 11:15:58 -0500

    Agrego color al borde de las tarjetas, y tambien agrego etiqueta requeria encima del nombre del personaje

--- src/modules/marvel-characters/components/CharacterCard/index.jsx
+++ src/modules/marvel-characters/components/CharacterCard/index.jsx
@@ -14,6 +14,7 @@ export default function CharacterCard({ image, name, isSkeleton }) {
         <img className="mvl-character-card__image" src={image} alt="" />
       </div>
       <div className="mvl-character-card__body">
+        <h6 className="etiqueta-marvel">1939 Timely Comics</h6>
         <h4 className="u-no-margin">{name}</h4>
       </div>
     </div>
