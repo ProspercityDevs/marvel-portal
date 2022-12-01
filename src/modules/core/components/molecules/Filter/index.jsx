bd40d65e42501a7567d1df555fd1738cc7964ece
charlie1978 <charlie.net@gmail.com>
2022-11-30 19:07:43 -0500

    modificaciones index.jsx y styles.scss de moleculas borde search bar

--- src/modules/core/components/molecules/Filter/index.jsx
+++ src/modules/core/components/molecules/Filter/index.jsx
@@ -1,7 +1,15 @@
 export default function Filter() {
   return (
-    <div className="mvl-character-gri-filters">
-      <input type="text" placeholder="search" />
+    <div id="srch" className="mvl-character-gri-filters">
+      <input
+        type="text"
+        value=""
+        autoComplete="off"
+        aria-autocomplete="list"
+        aria-controls="react-autowhatever-1"
+        className="typeahead__input"
+        placeholder="Search"></input>
+      {/* <input className="input" type="text" placeholder="search" onClick="cambiacolor" />*/}
     </div>
   );
 }
