ecd85bf64f37df8b1be9f6f4003d52451a8c71b5
LuisGT <luis.gomez.trujillo@.com>
2022-12-03 21:20:29 -0500

    Cambio en color de texto en paginacion

--- src/modules/marvel-characters/components/CharacterCard/index.jsx
+++ src/modules/marvel-characters/components/CharacterCard/index.jsx
@@ -15,7 +15,8 @@ export default function CharacterCard({ image, name, isSkeleton }) {
       </div>
       <div className="mvl-character-card__body">
         <h6 className="marvel-label">1939 Timely Comics</h6>
-        <h6>Current Year {new Date().getFullYear().toString()}</h6>
+        {/* <h6>Current Date {new Date().getFullYear().toString()}</h6> */}
+        <h6 className="marvel-label">{new Date().toDateString()}</h6>
         <h4 className="u-no-margin">{name}</h4>
         <h6 className="etiqueta-marvel">
           1939<br></br>Timely Comics
