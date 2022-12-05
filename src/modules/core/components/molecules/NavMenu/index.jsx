b0fe1ac522dd77cf19379568e304515fc6ddae6d
Jeisonlopez259911 <jeisonlopez2599@gmail.com>
2022-12-05 11:51:45 -0500

    Se agrego al navbar unas nuevas funciones

--- src/modules/core/components/molecules/NavMenu/index.jsx
+++ src/modules/core/components/molecules/NavMenu/index.jsx
@@ -5,12 +5,12 @@ export function NavMenu() {
   return (
     <nav className="mvl-navbar">
       <div className="mvl-navbar--left">
-        <NavLink className="mvl-navbar-item mlv-navbar__image" href="#">
+        <NavLink className="mvl-navbar-item mlv-navbar__image" href="#" to="/">
           <img src="/images/marvel_logo.png" className="mvl-navbar__img" alt="compny logo" />
         </NavLink>
 
         <div className="mvl-navbar__actions">
-          <NavLink className="mvl-navbar-item mvl-navbar-item--text" to="/">
+          <NavLink className="mvl-navbar-item mvl-navbar-item--text" to="/Characters">
             Characters
           </NavLink>
           <NavLink className="mvl-navbar-item mvl-navbar-item--text" to="/">
@@ -22,7 +22,7 @@ export function NavMenu() {
           <NavLink className="mvl-navbar-item mvl-navbar-item--text" to="/">
             Events
           </NavLink>
-          <NavLink className="mvl-navbar-item mvl-navbar-item--text" to="/">
+          <NavLink className="mvl-navbar-item mvl-navbar-item--text" to="/Series">
             Series
           </NavLink>
         </div>
