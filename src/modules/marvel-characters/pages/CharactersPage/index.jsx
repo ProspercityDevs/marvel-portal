3bee2b79e021a00c024c9c24ca1b5f15beb6799f
Jeorge Gonzalez <jeorgegova@outlook.com>
2022-12-05 00:27:29 -0500

    PM-19 se agrega fecha y hora y baground al card de personajes

--- src/modules/marvel-characters/pages/CharactersPage/index.jsx
+++ src/modules/marvel-characters/pages/CharactersPage/index.jsx
@@ -12,7 +12,8 @@ export function CharacterPage() {
 
       <div className="container">
         <section className="mvl-section-card">
-          <p>Use this space to add some cards and other content </p>
+          <h2> {clock()} </h2>
+          {/*<p>Use this space to add some cards and other content </p> */}
         </section>
 
         <h1>MARVEL CHARACTERS LIST</h1>
@@ -21,3 +22,13 @@ export function CharacterPage() {
     </div>
   );
 }
+
+function clock() {
+  const element = (
+    <div>
+      <h2>The time is: {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  return element;
+}
+setInterval(clock, 1000);
