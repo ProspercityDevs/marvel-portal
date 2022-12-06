c81f500cc6fee55df73607ebc03f33911dd53faf
Diani Aguirre <dianiyajanciflorez123@gmail.com>
2022-12-06 13:46:50 -0500

    se agrego nuevo contenedor,cambio de display en mvl-character-gri-filters

--- src/modules/core/components/molecules/Filter/index.jsx
+++ src/modules/core/components/molecules/Filter/index.jsx
@@ -1,14 +1,19 @@
 function Filter() {
   return (
     <div className="mvl-character-gri-filters">
-      <input
-        type="search"
-        placeholder="SEARCH"
-        className="input-nav"
-        value=""
-        autoComplete="off"
-        aria-autocomplete="list"
-        aria-controls="reac-autowhatever-1"></input>
+      <div>
+        <input
+          type="search"
+          placeholder="SEARCH"
+          className="input-nav"
+          value=""
+          autoComplete="off"
+          aria-autocomplete="list"
+          aria-controls="reac-autowhatever-1"></input>
+      </div>
+      <div>
+        <span className="span-results">3744 RESULTADOS</span>
+      </div>
     </div>
   );
 }
