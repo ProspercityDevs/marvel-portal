9b8284c52d989034dcd60c2b1e90d264bcb2569e
CamiloGiraldoM <camiloandresgiraldo000@gmail.com>
2022-12-05 20:17:02 -0500

    Corrijo errores de prettier

--- src/modules/marvel-characters/components/CharacterCard/index.jsx
+++ src/modules/marvel-characters/components/CharacterCard/index.jsx
@@ -17,7 +17,7 @@ export default function CharacterCard({ image, name, isSkeleton }) {
         <div className="mvl-character-card__body">
           <h6 className="marvel-label">1939 Timely Comics</h6>
           <h6 className="marvel-label">{new Date().toDateString()}</h6>
-          <h4 className="u-no-margin">{name}</h4>        
+          <h4 className="u-no-margin">{name}</h4>
         </div>
       </div>
     </div>
