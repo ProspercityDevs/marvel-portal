e72a16b4d45eac6aa55308cfd098a05eb643ec9f
charlie1978 <charlie.net@gmail.com>
2022-12-06 23:29:34 -0500

    Ref n3t cambios ramas

--- src/modules/marvel-characters/components/CharacterCard/index.jsx
+++ src/modules/marvel-characters/components/CharacterCard/index.jsx
@@ -15,9 +15,9 @@ export default function CharacterCard({ image, name, isSkeleton }) {
       </div>
       <div className="mvl-character-card__body-container">
         <div className="mvl-character-card__body">
+          <h4 className="u-no-margin">{name}</h4>
           <h6 className="marvel-label">1939 Timely Comics</h6>
           <h6 className="marvel-label">{new Date().toDateString()}</h6>
-          <h4 className="u-no-margin">{name}</h4>        
         </div>
       </div>
     </div>
