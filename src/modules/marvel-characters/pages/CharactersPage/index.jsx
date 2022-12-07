fa98c20e41485ba455fa8280b24823a141d528e3
charlie1978 <charlie.net@gmail.com>
2022-12-07 00:56:16 -0500

    se ajusta marco width imagenmarvel -> characters page. by CH4RL13.N3T

--- src/modules/marvel-characters/pages/CharactersPage/index.jsx
+++ src/modules/marvel-characters/pages/CharactersPage/index.jsx
@@ -11,9 +11,7 @@ export function CharacterPage() {
       </header>
 
       <div className="container">
-        <section className="mvl-section-card">
-         </section>
-
+        <section className="mvl-section-card"></section>
         <h1>MARVEL CHARACTERS LIST</h1>
         <CharactersGrid />
       </div>
