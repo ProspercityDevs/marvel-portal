52f05fe9de3a43a14152f1c03af02e01acee5bcd
Jeferson Hernandez <jefer.hernandez1@gmail.com>
2022-12-07 19:42:51 -0500

    PM-94: Titulo ok e images montadas ok

--- src/modules/marvel-characters/pages/CharactersPage/index.jsx
+++ src/modules/marvel-characters/pages/CharactersPage/index.jsx
@@ -11,8 +11,58 @@ export function CharacterPage() {
       </header>
 
       <div className="container">
+        <section className="Featured-Characters">
+          <div className="prueba">
+            <h1 className="Title-featured-characters">FEATURED CHARACTERS</h1>
+          </div>
+          <div className="Container-Cards-grip-6">
+            <div className="Cards-General-Characters">
+              <div className="Card-img">
+                <img
+                  className="Img-Cards"
+                  src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/107shr_ons_crd_03.jpg"></img>
+              </div>
+            </div>
+            <div className="Cards-General-Characters">
+              <div className="Card-img">
+                <img
+                  className="Img-Cards"
+                  src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/124irh_ons_crd_01.jpg"></img>
+              </div>
+            </div>
+            <div className="Cards-General-Characters">
+              <div className="Card-img">
+                <img
+                  className="Img-Cards"
+                  src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/276mbk_ons_crd_01.jpg"></img>
+              </div>
+            </div>
+            <div className="Cards-General-Characters">
+              <div className="Card-img">
+                <img
+                  className="Img-Cards"
+                  src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/110oky_ons_crd_04.jpg"></img>
+              </div>
+            </div>
+            <div className="Cards-General-Characters">
+              <div className="Card-img">
+                <img
+                  className="Img-Cards"
+                  src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/129nam_ons_crd_01.jpg"></img>
+              </div>
+            </div>
+            <div className="Cards-General-Characters">
+              <div className="Card-img">
+                <img
+                  className="Img-Cards"
+                  src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/277qrm_ons_crd_01.jpg"></img>
+              </div>
+            </div>
+          </div>
+        </section>
+
         <section className="mvl-section-card">
-          <p>Use this space to add some cards and other content </p>
+          <p>Use this space to add some cards and other content</p>
         </section>
 
         <h1>MARVEL CHARACTERS LIST</h1>
