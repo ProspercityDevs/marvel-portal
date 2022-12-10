051ca02b9280881bcf7703538ca0e8b824d94eb5
LuisGT <luis.gomez.trujillo@.com>
2022-12-10 09:08:14 -0500

    Se muestra total de resultados de API con correción temporal de warning con readonly en input

--- src/modules/core/components/molecules/NavMenu/index.jsx
+++ src/modules/core/components/molecules/NavMenu/index.jsx
@@ -5,12 +5,12 @@ export function NavMenu() {
   return (
     <nav className="mvl-navbar">
       <div className="mvl-navbar--left">
-        <NavLink className="mvl-navbar-item mlv-navbar__image" href="#">
+        <NavLink className="mvl-navbar-item mlv-navbar__image" to="/">
           <img src="/images/marvel_logo.png" className="mvl-navbar__img" alt="compny logo" />
         </NavLink>
 
         <div className="mvl-navbar__actions">
-          <NavLink className="mvl-navbar-item mvl-navbar-item--text" to="/">
+          <NavLink className="mvl-navbar-item mvl-navbar-item--text" to="/characters">
             CHARACTERS
           </NavLink>
           <NavLink className="mvl-navbar-item mvl-navbar-item--text" to="/comics" target="_blank">
