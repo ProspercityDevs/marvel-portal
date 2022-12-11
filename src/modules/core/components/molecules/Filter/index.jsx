2845336cc0a154bafbe34fb2d1dda9ba7e906ecf
Jeorge Gonzalez <jeorgegova@outlook.com>
2022-12-11 17:47:22 -0500

    Uso de la libreria multiselect para crear filtro de busqueda por Movie

--- src/modules/core/components/molecules/Filter/index.jsx
+++ src/modules/core/components/molecules/Filter/index.jsx
@@ -1,76 +1,35 @@
+import Multiselect from 'multiselect-react-dropdown';
+import { useState } from 'react';
+
 export default function Filter() {
-  // const selectBtn = document.querySelector('.select-btn'),
-  //   items = document.querySelectorAll('.item');
-
-  // selectBtn.addEventListener('click', () => {
-  //   selectBtn.classList.toggle('open');
-  // });
-
-  // items.forEach((item) => {
-  //   item.addEventListener('click', () => {
-  //     item.classList.toggle('checked');
-
-  //     let checked = document.querySelectorAll('.checked'),
-  //       btnText = document.querySelector('.btn-text');
-
-  //     if (checked && checked.length > 0) {
-  //       btnText.innerText = `${checked.length} Selected`;
-  //     } else {
-  //       btnText.innerText = 'Select Language';
-  //     }
-  //   });
-  // });
-
+  const [movie] = useState([
+    'ANt-MAN',
+    'ANT-MAN AND THE WASP',
+    'ARMOR WARS',
+    'AVENGERS: AGE OF ULTRON',
+    'AVENGER: INFINITY WAR',
+    'BLACK PANTER',
+    'BLACK PANTER: WAKANDA FOREVER'
+  ]);
   return (
     <div className="mvl-character-gri-filters">
       <input type="text" placeholder="search" />
-
       <div className="container-checked">
-        <div className="select-btn">
-          <span className="btn-text">Movie</span>
-          <span className="arrow-dwn">
-            <i className="fa-solid fa-chevron-down"></i>
-          </span>
-        </div>
-
-        <ul className="list-items">
-          <li className="item">
-            <span className="checkbox">
-              <i className="fa-solid fa-check check-icon"></i>
-            </span>
-            <span className="item-text">HTML & CSS</span>
-          </li>
-          <li className="item">
-            <span className="checkbox">
-              <i className="fa-solid fa-check check-icon"></i>
-            </span>
-            <span className="item-text">Bootstrap</span>
-          </li>
-          <li className="item">
-            <span className="checkbox">
-              <i className="fa-solid fa-check check-icon"></i>
-            </span>
-            <span className="item-text">JavaScript</span>
-          </li>
-          <li className="item">
-            <span className="checkbox">
-              <i className="fa-solid fa-check check-icon"></i>
-            </span>
-            <span className="item-text">Node.Js</span>
-          </li>
-          <li className="item">
-            <span className="checkbox">
-              <i className="fa-solid fa-check check-icon"></i>
-            </span>
-            <span className="item-text">React JS</span>
-          </li>
-          <li className="item">
-            <span className="checkbox">
-              <i className="fa-solid fa-check check-icon"></i>
-            </span>
-            <span className="item-text">Mango DB</span>
-          </li>
-        </ul>
+        <Multiselect
+          isObject={false}
+          onKeyPressFn={function noRefCheck() {}}
+          onRemove={(event) => {
+            console.log(event);
+          }}
+          onSearch={(event) => {
+            console.log(event);
+          }}
+          onSelect={(event) => {
+            console.log(event);
+          }}
+          options={movie}
+          showCheckbox
+        />
       </div>
     </div>
   );
