77b566bdced7429a73b8739026b225e8513f54e3
Jose <Jose16mar@gmail.com>
2022-12-12 12:15:01 -0500

    historia de usuario PM-141 V1

--- src/modules/core/components/molecules/Filter/index.jsx
+++ src/modules/core/components/molecules/Filter/index.jsx
@@ -1,8 +1,15 @@
 import './styles.scss';
+import propTypes from 'prop-types';
 
-export default function Filter() {
+Filter.propTypes = {
+  totalItems: propTypes.number.isRequired
+};
+
+export default function Filter({ totalItems }) {
   return (
     <div className="mvl-character-gri-filters">
+      <h5>{totalItems}</h5>
+      <span>{totalItems}</span>
       <input className="mvl-character-gri-input" type="text" placeholder="search" />
     </div>
   );
