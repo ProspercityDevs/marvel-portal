abeadbb874f656d858d589637b481a29999d1a2e
Jose <Jose16mar@gmail.com>
2022-12-12 14:13:17 -0500

    historia de usuario PM-141 V2 se corrige un conflicto y se termina la PM-141

--- src/modules/core/components/molecules/Filter/index.jsx
+++ src/modules/core/components/molecules/Filter/index.jsx
@@ -9,7 +9,23 @@ Filter.propTypes = {
 export default function Filter({ totalItems }) {
   return (
     <div className="mvl-character-gri-filters">
-      <input className="mvl-character-gri-input" type="text" placeholder="search" />
+      <div className="col">
+        <div className="contenerdor_1">
+          <input className="mvl-character-gri-input" type="text" placeholder="search" />
+        </div>
+      </div>
+
+      <div className="col">
+        <div className="contenerdor_2">
+          <MovieFilter />
+        </div>
+      </div>
+
+      <div className="col">
+        <div className="contenedor_totalitems">
+          <h4>Number of characters: {totalItems}</h4>
+        </div>
+      </div>
     </div>
   );
 }
