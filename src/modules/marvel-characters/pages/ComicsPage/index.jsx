58884097789fa6d535fd8f653ffec4c5bf8f4a4d
BrFuentes <brayantigiza@gmail.com>
2022-12-12 10:58:22 -0500

    creacion de las diferentes rutas del menu

---
+++ src/modules/marvel-characters/pages/ComicsPage/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ComicsPage(){
+    return(
+        <div>
+            <h1>ComicsPage</h1>
+        </div>
+        
+    )
+}
+export {ComicsPage}
---
+++ src/modules/marvel-characters/pages/SeriesPage/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SeriesPage(){
+    return(
+        <div>
+            <h1>SeriesPage</h1>
+        </div>
+        
+    )
+}
+export {SeriesPage}
