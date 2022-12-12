11987224236f31af6517994aecce1be464967337
LuisGT <luis.gomez.trujillo@.com>
2022-12-12 16:23:34 -0500

    Merge con cambios de CharacterProm y Footer

--- src/modules/marvel-characters/pages/CharactersPage/index.jsx
+++ src/modules/marvel-characters/pages/CharactersPage/index.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import CharactersGrid from '@/modules/marvel-characters/components/CharacterGrid';
+import FeaturedCharacters from '../../components/FeaturedCharacteres';
 import './styles.scss';
 import CharacterProm from '@/modules/core/components/molecules/CharacterProm';
 
 export function CharactersPage() {
   return (
-    <div className="mvl-characters-page">
-      <header className="mvl-characters-page-header">
-        <h1 className="u-no-margin">MARVELS CHARACTERS</h1>
-        <p>Get a list of all Marvels characters and villians</p>        
-      </header>
-
-      <div className="container">
-
-        <section className="mvl-section-card"></section>
-        <h1>FEATURED CHARACTERS</h1>
-        <FeaturedCharacters />
-        <h1>MARVEL CHARACTERS LIST</h1>
-        <CharactersGrid />
+    <>
+      <div className="mvl-characters-page">
+        <header className="mvl-characters-page-header">
+          <h1 className="u-no-margin">MARVELS CHARACTERS</h1>
+          <p>Get a list of all Marvels characters and villians</p>
+        </header>
+        <div className="container">
+          <section className="mvl-section-card"></section>
+          <h1>FEATURED CHARACTERS</h1>
+          <FeaturedCharacters />
+          <h1>MARVEL CHARACTERS LIST</h1>
+          <CharactersGrid />
+        </div>
       </div>
-    </div>
+      <CharacterProm />
+    </>
   );
 }
