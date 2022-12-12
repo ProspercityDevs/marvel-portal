f1ca8725438ef135122bb5d6f470e4f2fe6eb8a5
Jeorge Gonzalez <jeorgegova@outlook.com>
2022-12-11 23:59:41 -0500

    actualizacion de la historia PM-132

--- src/modules/core/components/molecules/BeforFooter/index.jsx
+++ src/modules/core/components/molecules/BeforFooter/index.jsx
@@ -3,11 +3,9 @@ import './styles.scss';
 export default function BeforeFooter() {
   return (
     <div className="mvl-before-footer">
-      <div className="contenedor_left">
-        <h1>first div</h1>
-      </div>
+      <div className="contenedor_left"> </div>
       <div className="contenedor_right">
-        <h1>secund div</h1>
+        <h3>MARVEL INSIDER</h3>
       </div>
     </div>
   );
