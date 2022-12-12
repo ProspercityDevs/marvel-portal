bc45be2bc808863b4d2042a0c7208f6a3d6dc30f
Jeorge Gonzalez <jeorgegova@outlook.com>
2022-12-11 22:04:07 -0500

    inicio desarrollo del before footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import '@/assets/styles/main.scss';
 
 import { NavMenu } from '@/modules/core/components/molecules/NavMenu';
 import MainFooter from '@/modules/core/components/molecules/MainFooter';
+import BeforeFooter from './modules/core/components/molecules/BeforFooter';
 
 import AppRoutes from './routes';
 
@@ -10,6 +11,7 @@ function App() {
     <div className="main">
       <NavMenu />
       <AppRoutes />
+      <BeforeFooter />
       <MainFooter />
     </div>
   );
