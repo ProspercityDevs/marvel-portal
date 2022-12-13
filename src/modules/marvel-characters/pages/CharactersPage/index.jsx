83959db5074cdcaa1d688860ea91ac2cf7378966
= <=>
2022-12-12 19:46:05 -0500

    nueva corrección para nir ramas

---
+++ src/modules/marvel-characters/pages/CharactersPage/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import CharactersGrid from '@/modules/marvel-characters/components/CharacterGrid';
+import './styles.scss';
+
+export function CharacterPage() {
+  return (
+    <div className="mvl-characters-page">
+      <header className="mvl-characters-page-header">
+        <h1 className="u-no-margin">MARVELS CHARACTERS</h1>
+        <p>Get a list of all Marvels characters and villians</p>
+      </header>
+
+      <div className="container">
+        <section className="mvl-section-card">
+          <p> **ALPHA TEAM**{new Date().toLocaleDateString()} </p>
+        </section>
+
+        <h1>MARVEL CHARACTERS LIST</h1>
+        <CharactersGrid />
+      </div>
+    </div>
+  );
+}
