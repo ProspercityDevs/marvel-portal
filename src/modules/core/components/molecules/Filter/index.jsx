51d4dd1616c19a547dc62bf26f200bf0d19afb30
juan jose <juansecardonaramirez@gmail.com>
2022-12-15 19:40:39 -0500

    agregue nuevas funciones

--- src/modules/core/components/molecules/Filter/index.jsx
+++ src/modules/core/components/molecules/Filter/index.jsx
@@ -1,15 +1,35 @@
+import "./styles.scss";
+
 function Filter() {
+
   return (
     <div className="mvl-character-gri-filters">
-      <input
-        type="search"
-        placeholder="SEARCH"
-        className="input-nav"
-        value=""
-        autoComplete="off"
-        aria-autocomplete="list"
-        aria-controls="reac-autowhatever-1"></input>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="search">
+              <input type="text" className="color_f" placeholder="SEARCH" />
+            </div>
+          </div>
+          <div className="col">
+            <div className="filtro2"></div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="search">
+              <input type="text" className="color_f" placeholder="SEARCH" />
+            </div>
+          </div>
+          <div className="col">
+            <div className="filtro2"></div>
+          </div>
+        </div>
+
+      </div>
+
     </div>
+
   );
 }
 export default Filter;
