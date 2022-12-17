531245e4fe4b9ef3addd935d3d7a9ce2e644e184
Jeorge Gonzalez <jeorgegova@outlook.com>
2022-12-17 01:19:10 -0500

    se agregan estilos y fuentes a la pagia 404 y se inicia el desarrollo del ojo

--- src/modules/core/pages/NotFoundPage404.jsx
+++ src/modules/core/pages/NotFoundPage404.jsx
@@ -3,7 +3,13 @@ import './NotFoundPage404.scss';
 const NotFoundPage404 = () => {
   return (
     <div className="page404">
-      <h1>404 Not found page</h1>
+      <div className="text404">
+        <div className='contenedor1'><h1>404 NOT PAGE FOUND</h1></div>
+        <div className='contenedor2'><h2>Not even the Eye of Uatu sees your request...</h2></div>
+        <div className='contenedor3'><h4>Check that you type the address corrently, go back to your previous<br></br>page or try usin our site search to find something specific</h4></div>
+        
+      </div>
+      <div className="image404"></div>
     </div>
   );
 };
