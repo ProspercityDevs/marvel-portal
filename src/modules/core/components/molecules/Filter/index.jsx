44882690edac77d4922f014ed3fb2319870ca671
Jeisonlopez259911 <jeisonlopez2599@gmail.com>
2022-12-17 17:29:31 -0500

    Hubo mejoras en el codigo

--- src/modules/core/components/molecules/Filter/index.jsx
+++ src/modules/core/components/molecules/Filter/index.jsx
@@ -9,21 +9,29 @@ Filter.propTypes = {
 export default function Filter({ totalItems }) {
   return (
     <div className="mvl-character-gri-filters">
-      <div className="col">
-        <div className="contenerdor_1">
-          <input className="mvl-character-gri-input" type="text" placeholder="search" />
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <input className="mvl-character-gri-input" type="text" placeholder="search" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="contenedor_totalitems">
+              <h4 className="total">{totalItems} RESULTS</h4>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="col">
-        <div className="contenerdor_2">
-          <MovieFilter />
-        </div>
-      </div>
-
-      <div className="col">
-        <div className="contenedor_totalitems">
-          <h4>Number of characters: {totalItems}</h4>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="filtros">
+              <MovieFilter />
+            </div>
+            <hr className="linea" />
+          </div>
         </div>
       </div>
     </div>
