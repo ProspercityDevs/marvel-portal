0a42a8a7aa5fa90bfeb253ff44991266be7b432a
BrFuentes <brayantigiza@gmail.com>
2022-12-19 19:28:33 -0500

    empezando a utilizar React pagination

--- src/modules/core/components/molecules/Paginator/index.jsx
+++ src/modules/core/components/molecules/Paginator/index.jsx
@@ -1,8 +1,7 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useEffect,useState } from 'react';
 import PropTypes from 'prop-types';
+import ReactPaginate from 'react-paginate';
 
-import { BiChevronRight, BiChevronLeft } from 'react-icons/bi';
 import './styles.scss';
 
 Pagination.propTypes = {
@@ -12,77 +11,39 @@ Pagination.propTypes = {
   onPageChange: PropTypes.func
 };
 
-export default function Pagination({ totalItems, pageSize = 10, initialPage = 1, onPageChange }) {
-  const totalPages = getFlooredToatalPages(totalItems, pageSize);
-  const [currentPage, setCurrentPage] = useState(initialPage);
-
-  function onNextpageClick() {
-    const nextPage = getNextPage(currentPage, totalPages);
-    setCurrentPage(nextPage);
-  }
-  function onNextNextpageClick() { 
-    const nextNextPage = getNextPage(currentPage+1, totalPages);
-    setCurrentPage(nextNextPage);
-  }
-
-  function onNextTriplepageClick() {
-    const nextTriplePage = getNextPage(currentPage+2, totalPages);
-    setCurrentPage(nextTriplePage);
-  }
-
-  function onPreviousPageClick() {
-    const prevPage = getPreviousePage(currentPage);
-    setCurrentPage(prevPage);
-  }
-  function onFinalPage(){
-    setCurrentPage(totalPages);
-  }
-  useEffect(() => {
-    onPageChange(currentPage);
-  }, [currentPage]);
+export default function Pagination({ totalItems, pageSize = 24, initialPage = 1, onPageChange }) {
  
+  const [itemOffset, setItemOffset] = useState(initialPage);
+  const currentPage = itemOffset + pageSize;
+  const pageCount = Math.ceil(totalItems/pageSize);
+
+  
+  const handlePageClick = () => {
+    useEffect(() => {
+      onPageChange(currentPage);
+    }, [currentPage]);
+  
+    const newOffset = (event.selected * pageSize) % totalItems.length;
+    setItemOffset(newOffset);
+  };
+
   return (
-    <div className="mvl-paginator">
-    
-      <div className="mvl-paginator__actions">
-        <button disabled={isFirstPage(currentPage)} onClick={onPreviousPageClick}>
-          <BiChevronLeft />
-        </button>
-        <button className='page-active'>{currentPage}</button>
-        <button disabled={isLastPage(currentPage, totalPages)} onClick={onNextpageClick}>{currentPage+1}</button>
-        <button disabled={isLastPage(currentPage, totalPages)} onClick={ onNextNextpageClick}>{currentPage+2}</button>
-        <button disabled={isLastPage(currentPage, totalPages)} onClick={ onNextTriplepageClick}>{currentPage+3}</button>
-        <button>...</button>
-        <button disabled={isLastPage(currentPage, totalPages)} onClick={onFinalPage}>{totalPages}</button>
-       
-        <button disabled={isLastPage(currentPage, totalPages)} onClick={onNextpageClick}>
-          <BiChevronRight />
-        </button>
-      </div>
-    </div>
+    <>
+
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        containerClassName = "pagination"
+        nextLinkClassName='page-num'
+        pageLinkClassName='page-num'
+        previousLinkClassName='page-num'
+        activeLinkClassName='page-num'
+      />
+    </>
   );
-}
-
-function isFirstPage(currentPage) {
-  return currentPage === 1;
-}
-
-function isLastPage(currentPage, maxPage) {
-  return currentPage === maxPage;
-}
-
-function getPreviousePage(currentPage) {
-  return currentPage > 1 ? currentPage - 1 : currentPage;
-}
-
-function getNextPage(currentPage, maxPage) {
-  return currentPage < maxPage ? currentPage + 1 : maxPage;
-}
-
-function getFlooredToatalPages(totalItems, pageSize) {
-  return Math.floor(getToatalPages(totalItems, pageSize));
-}
-
-function getToatalPages(totalItems, pageSize) {
-  return totalItems / pageSize;
 }
