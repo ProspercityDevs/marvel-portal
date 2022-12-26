f6326689b56b093707491e06d37bc97573180b84
BrFuentes <brayantigiza@gmail.com>
2022-12-25 21:22:32 -0500

    cambio de nombres de variables para la api de series

---
+++ src/modules/marvel-characters/components/CharacterGrid/seriesGrid.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+
+import { getSeriesForGrid } from '../../services/series/series.api.js';
+
+const ITEMS_PER_PAGE = 24;
+
+export function SeriesGridPaginated() {
+  const [series, setSeries] = useState([]);
+  useEffect(() => {
+    fetchSeriesAtPage();
+  }, []);
+  async function fetchSeriesAtPage(page = 1) {
+    const domain=`series`
+    
+    const data = await getSeriesForGrid(page, ITEMS_PER_PAGE, domain);
+
+    setSeries(data.results);
+    
+  }
+  
+  return series
+  };
+
+  
---
+++ src/modules/marvel-characters/services/series/series.api.js
@@ -0,0 +1,28 @@
+import { getAllPaginated } from '@/modules/core/services/marvel.api-rest';
+import { defaultMapper } from '@/modules/core/services';
+
+export function getSeriesForGrid(page, itemsPerPage, domain) {
+
+  return getSeriesAtPage(page, itemsPerPage, domain, {
+    mappedBy: mapSeriesToCard
+  });
+}
+
+async function getSeriesAtPage(
+  page,
+  itemsPerPage,
+  domain,
+  { mappedBy = defaultMapper, queryParams = {} }
+) {
+  return getAllPaginated(domain, page, {
+    mappedBy,
+    itemsPerPage,
+    ...queryParams
+  });
+}
+
+function mapSeriesToCard(series) {
+  return {
+    name: series.title,
+  };
+}
