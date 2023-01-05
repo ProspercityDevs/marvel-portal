67d5c51932b8ed8c079976875bc1a6991b0c728c
CamiloGiraldoM <camiloandresgiraldo000@gmail.com>
2023-01-05 15:25:13 -0500

    Revert "Elimino componente movie"
    This reverts commit ef4637e644c4db8099a2d67af53eb01eb58dc653.

--- src/modules/marvel-characters/components/CharacterGrid/index.jsx
+++ src/modules/marvel-characters/components/CharacterGrid/index.jsx
@@ -7,6 +7,7 @@ import CharacterCard from '@/modules/marvel-characters/components/CharacterCard'
 import Paginator from '@/modules/core/components/molecules/Paginator';
 import './styles.scss';
 import TotalResults from 'src/modules/core/components/atoms/TotalResults';
+import MovieFilter from 'src/modules/core/components/molecules/MovieFilter';
 import SerieFilter from 'src/modules/core/components/molecules/SerieFilter';
 
 const INITIAL_PAGE = 1;
@@ -66,6 +67,7 @@ export default function CharacterGridPaginated({ searchValue }) {
     <>
       <TotalResults totalItems={totalItems} />
       {/* <Filter query={queryParams} onQueryChange={onQueryChange} /> */}
+      <MovieFilter />
       <SerieFilter />
       <div className="mvl-grid mvl-grid-6">
         <CharacterGrid
