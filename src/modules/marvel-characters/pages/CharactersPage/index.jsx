010a503b001ecdd996cdf7eb7658a7ff1b87d7fe
LuisGT <luis.gomez.trujillo@gmail.com>
2023-01-06 23:50:57 -0500

    Merge con Luis_Dev

--- src/modules/marvel-characters/pages/CharactersPage/index.jsx
+++ src/modules/marvel-characters/pages/CharactersPage/index.jsx
@@ -22,7 +22,7 @@ export function CharactersPage() {
   };
 
   function SelectOption( {selectValue} ) {
-      const option=1;  
+      //const option=1;  
       if (selectValue == 'A-Z') {
         return (
         <> 
